1f119f7af0fb3c9ee57c9bc94d1fd08bdbeeaf52
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-05-06 09:33:20 +0300

    Python sequences Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # build the sequence
+    if length <= 0:
+        seq = []
+    else:
+        seq = [0]
+        if length > 1:
+            seq.append(1)
+        for _ in range(2, length):
+            seq.append(seq[-1] + seq[-2])
+
+    print(seq)
+
